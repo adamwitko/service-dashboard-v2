dbb013109784987137c40eceaaa4887021ff03e0
Adam Witko <adam.witko@s4rb.com>
2019-10-17 14:42:05 +0100

    functions

--- src/components/Tableau.tsx
+++ src/components/Tableau.tsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Typography from '@material-ui/core/Typography'
+import axios from 'axios'
+import { Loading } from './Loading'
 
 export function Tableau() {
-  return <Typography component="p">Hello, Tableau</Typography>
+  const [name, setName] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true)
+      const result = await axios('/.netlify/functions/data')
+
+      setName(result.data)
+      setIsLoading(false)
+    }
+
+    fetchData()
+  }, [])
+
+  if (isLoading) {
+    return <Loading />
+  }
+
+  return <Typography component="p">{name}</Typography>
 }
