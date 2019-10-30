00136ee8f63275eb3a852ad3aeda5faf7879f661
Adam Witko <adam.witko@s4rb.com>
2019-10-30 15:33:04 +0000

    WIP graphql Netlify functions

--- src/components/Tableau.tsx
+++ src/components/Tableau.tsx
@@ -1,27 +1,20 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import Typography from '@material-ui/core/Typography'
-import axios from 'axios'
 import { Loading } from './Loading'
+import { useQuery } from 'graphql-hooks'
+
+const helloQuery = `query { hello }`
 
 export function Tableau() {
-  const [name, setName] = useState('')
-  const [isLoading, setIsLoading] = useState(false)
+  const { loading, error, data } = useQuery(helloQuery)
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true)
-      const result = await axios('/.netlify/functions/data')
-
-      setName(result.data)
-      setIsLoading(false)
-    }
-
-    fetchData()
-  }, [])
-
-  if (isLoading) {
+  if (loading) {
     return <Loading />
   }
 
-  return <Typography component="p">{name}</Typography>
+  if (error) {
+    return <p>Oh noes</p>
+  }
+
+  return <Typography component="p">{data.hello}</Typography>
 }
